82873e5df98d18fb913e179e7c49a49e77132b24
[SerVigil] <[sergio_vigil@hotmail.com]>
2024-10-25 19:06:04 +0200

    Primer Commit

---
+++ src/Despedida.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function Despedida(props)
+{
+    return <h1>¡Hasta pronto!</h1>
+}
+
+export default Despedida;
---
+++ src/Saludos.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Saludo()
+{
+    return <h1>¡Hola, qué tal estás!</h1>
+}
+    export default Saludo;
